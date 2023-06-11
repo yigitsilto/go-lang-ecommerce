7d442718b21786fd57131678e2f2df254fa3b36e
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2023-06-11 23:17:23 +0300

    ecommerce home page

---
+++ Repositories/RelatedProductRepository.go
@@ -0,0 +1,35 @@
+package Repositories
+
+import (
+	"ecommerce/database"
+	model "ecommerce/models"
+)
+
+func GetAllRelatedProducts() ([]model.RelatedProductsModel, error) {
+
+	popularProducts := []model.RelatedProductsModel{}
+
+	err := database.Database.Table("popular_products").
+		Select(
+			"products.id, products.slug, products.short_desc, products.price, products.special_price, products.qty, products.in_stock," +
+				"GROUP_CONCAT(DISTINCT brt.name) AS brand_name, pt.name, " +
+				"GROUP_CONCAT(DISTINCT f.path) AS file_paths, products.is_active, popular_products.created_at, popular_products.updated_at",
+		).
+		Joins(
+			"INNER JOIN products ON products.id = popular_products.product_id " +
+				"INNER JOIN product_translations pt ON pt.product_id = products.id " +
+				"LEFT JOIN entity_files ef ON ef.entity_type = 'Modules\\\\Product\\\\Entities\\\\Product' AND ef.entity_id = products.id " +
+				"LEFT JOIN files f ON f.id = ef.file_id " +
+				"INNER JOIN brands br ON br.id = products.brand_id " +
+				"INNER JOIN brand_translations brt ON brt.brand_id = br.id",
+		).
+		Group(
+			"products.id, products.slug, products.short_desc, products.price, products.special_price, products.qty, products.in_stock, " +
+				"pt.name, products.is_active, popular_products.created_at, popular_products.updated_at",
+		).
+		Limit(20).
+		Find(&popularProducts).Error
+
+	return popularProducts, err
+
+}
--- services/HomePageService.go
+++ services/HomePageService.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"ecommerce/Repositories"
 	"ecommerce/database"
 	model "ecommerce/models"
 	"fmt"
@@ -9,19 +10,7 @@ import (
 
 func GetHomePage() (model.HomePageModel, error) {
 
-	popularProducts := []model.RelatedProductsModel{}
-
-	err := database.Database.Table("popular_products").Select(
-		"products.id, products.slug, products.short_desc, " +
-			"products.price, products.special_price, products.qty, products.in_stock, brt.name as brand_name," +
-			" pt.name, f.path, products.is_active, popular_products.created_at, popular_products.updated_at ",
-	).Joins(
-		"INNER JOIN products ON products.id = popular_products.product_id " +
-			" INNER JOIN product_translations pt on pt.product_id = products.id " +
-			"LEFT JOIN entity_files ef on ef.entity_type = 'Modules\\\\Product\\\\Entities\\\\Product' and ef.entity_id = products.id" +
-			" LEFT JOIN files f on f.id = ef.file_id INNER JOIN brands br ON br.id = products.brand_id " +
-			"INNER JOIN brand_translations brt ON brt.brand_id = br.id  ",
-	).Limit(20).Find(&popularProducts).Error
+	popularProducts, err := Repositories.GetAllRelatedProducts()
 
 	buildPopularProducts(popularProducts)
 
