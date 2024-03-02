76518d7d65a9a7967200bfa53850592e7b88f6bf
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-03-02 19:19:52 +0300

    docker bf

---
+++ build.sh
@@ -0,0 +1,12 @@
+docker build -t go-ecommerce-image .
+
+minikube image load go-ecommerce-image
+
+
+# kubectl apply -f service.yaml
+
+# kubectl apply -f deployment.yaml
+
+# minikube service gocommerce-service
+
+
