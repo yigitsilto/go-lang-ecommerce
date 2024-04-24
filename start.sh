6c98e558e55ad91fcd0e3f56193c7fb0da92fb61
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-04-24 14:57:23 +0300

    docker bf

--- start.sh
+++ start.sh
@@ -7,6 +7,9 @@ if [ ! -f .env ]; then
   elif [ "$(git rev-parse --abbrev-ref HEAD)" = "peynirciler" ]; then
     cp env/.peynircilerenv .env
   fi
+ elif [ "$(git rev-parse --abbrev-ref HEAD)" = "proshop" ]; then
+     cp env/.proenv .env
+   fi
 fi
 
 
@@ -17,10 +20,15 @@ if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]; then
 elif [ "$(git rev-parse --abbrev-ref HEAD)" = "peynirciler" ]; then
     docker stack rm gopeynirciler
     docker build -t gocommercepeynirciler --no-cache -f Dockerfile .
+elif [ "$(git rev-parse --abbrev-ref HEAD)" = "proshop" ]; then
+    docker stack rm gopro
+    docker build -t gocommercepro --no-cache -f Dockerfile .
 fi
 
 if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]; then
   docker stack deploy -c docker-compose-kore.yaml gokore --resolve-image=always
 elif [ "$(git rev-parse --abbrev-ref HEAD)" = "peynirciler" ]; then
   docker stack deploy -c docker-compose-peynirciler.yaml gopeynirciler --resolve-image=always
+elif [ "$(git rev-parse --abbrev-ref HEAD)" = "proshop" ]; then
+  docker stack deploy -c docker-compose-pro.yaml gopro --resolve-image=always
 fi
